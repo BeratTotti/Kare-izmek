c90b08f2184af284f824c1ea8f1491d58e2fd1f8
Berat Bilgehan Beter <berattotibonbon@gmail.com>
2023-08-01 19:24:49 +0300

    Kare Çizmek

---
+++ main.py
@@ -0,0 +1,16 @@
+import turtle
+from turtle import  *
+
+drawing_board = turtle.Screen()
+drawing_board.bgcolor("red")
+drawing_board.title("Kare Çizmek")
+
+turtle_instance = turtle.Turtle()
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle_instance.left(90)
+turtle_instance.forward(200)
+turtle.done()
